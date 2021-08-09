121a57baffa2ede58c990a70183ac1d65afe2e31
ehsan <asgari@berkeley.edu>
2021-08-09 10:59:57 +0200

    simplify the code

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ if __name__ == "__main__":
         simplebot = SimpleBot(config['mybot']['user'],config['mybot']['password'],config['mybot']['server'])
         simplebot.run()
 
-    if len(config['adminbot']['user'].strip())>0:
-        adminbot = AdminBot(config['adminbot']['user'],config['adminbot']['password'],config['adminbot']['server'],config['adminbot']['admins'].split(","),config['adminbot']['synapse_home'])
-        adminbot.run()
-    
-    if len(config['grpbot']['user'].strip())>0:
-        groupsyncbot = GroupSyncBot(config['grpbot']['user'],config['grpbot']['password'],config['grpbot']['server'],config['grpbot']['admins'].split(","),config['grpbot']['synapse_home'])
-        groupsyncbot.run()
     # Infinitely read stdin to stall main thread while the bot runs in other threads
     while True:
         input()
